d978db4a89dd479302117bebd994c32f5ef460db
Brian Crocker <briancrocker81@gmail.com>
2018-04-10 23:37:43 +0100

    Added feature to take user data to create a branch in another repo

--- convert_jira.rb
+++ convert_jira.rb
@@ -1,6 +1,6 @@
 # require 'rubygems'
 # require 'jira-ruby'
-# require_relative 'config.rb'
+require_relative 'config.rb'
 #
 # options = {
 #   :username     => $username,
@@ -48,18 +48,22 @@
 #
 # puts $arr.length
 class Branch
+  attr_accessor :num, :branch_type, :branch_name # Possibility these can be removed after testing
 
-  def initialize()
-    puts 'Enter branch number'
-    num = gets.chomp
+  def initialize(params = {})
+    # puts 'Enter branch number'
+    # num = gets.chomp
+    num = params.fetch(:num)
     validate_num(num)
 
-    puts 'Enter branch type. \nEnter b for Bug or f for Feature'
-    branch_type = gets.chomp
+    # puts 'Enter branch type. \nEnter b for Bug or f for Feature'
+    # branch_type = gets.chomp
+    branch_type = params.fetch(:branch_type)
     validate_branch_type(branch_type)
 
-    puts 'Enter branch title'
-    branch_name = gets.chomp
+    # puts 'Enter branch title'
+    # branch_name = gets.chomp
+    branch_name = params.fetch(:branch_name)
     validate_branch_name(branch_name)
 
     convert
@@ -76,27 +80,40 @@ class Branch
   end
 
   def validate_branch_type(branch_type)
-    @branch_type = branch_type.downcase
-    if @branch_type == 'b' || @branch_type == 'bug'
-      @branch_type = 'bug'
-    elsif @branch_type == 'f' || @branch_type == 'feature'
-      @branch_type = 'feature'
-    else
-      puts 'Enter branch type. \nEnter b for Bug or f for Feature'
-      branch_type = gets.chomp
-      validate_branch_type(branch_type)
-    end
+    @branch_type = branch_type
+    # @branch_type = branch_type.downcase
+    # if @branch_type == 'b' || @branch_type == 'bug'
+    #   @branch_type = 'bug'
+    # elsif @branch_type == 'f' || @branch_type == 'feature'
+    #   @branch_type = 'feature'
+    # else
+    #   puts "Enter branch type. \nEnter b for Bug or f for Feature"
+    #   branch_type = gets.chomp
+    #   validate_branch_type(branch_type)
+    # end
     self
   end
 
   def validate_branch_name(branch_name)
-    @branch_name = branch_name.downcase.strip.gsub('/','-').gsub(' ', '-').gsub(/[^\w-]/, '').capitalize
+    @branch_name = branch_name.to_s.downcase.strip.gsub('/','-').gsub(' ', '-').gsub(/[^\w-]/, '').capitalize
     self
   end
 
+  def process_git
+    Kernel::system "git --git-dir=#{$path_repo}/.git --work-tree=#{$path_repo} checkout master"
+    Kernel::system "git --git-dir=#{$path_repo}/.git --work-tree=#{$path_repo} fetch origin"
+    Kernel::system "git --git-dir=#{$path_repo}/.git --work-tree=#{$path_repo} checkout -b #{@new_branch}"
+  end
+
   def convert
-    puts "#{@branch_type}/FLX-#{@num}-#{@branch_name}"
+    @new_branch = "#{@branch_type}/FLX-#{@num}-#{@branch_name}"
+    process_git
   end
 
 end
-branch = Branch.new
+
+b_num = rand(10 ** 4)
+lorem = ["wibble","dibble","fibble","bibble","gibble","aibble","sibble","cibble","xibble","tibble","oibble","libble"].sample(4)
+bch = ["bug", "feature"].sample
+
+branch = Branch.new(:num => b_num.to_s, :branch_type => bch.to_s, :branch_name => lorem.to_s )
